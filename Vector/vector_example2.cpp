16b68ff35e7ca508a1766f800b5a51a9d741c87f
Kim Do Hyun <dohyun9565@gmail.com>
2020-06-02 23:49:03 +0900

    iterator(반복자) 사용법

---
+++ Vector/vector_example2.cpp
@@ -0,0 +1,22 @@
+// https://modoocode.com/174
+#include <iostream>
+#include <vector>
+
+int main()
+{
+	std::vector<int> vec;
+	vec.push_back(10);
+	vec.push_back(20);
+	vec.push_back(30);
+	vec.push_back(40);
+
+	// 반복자 사용
+	for (std::vector<int>::iterator itr = vec.begin(); itr != vec.end(); ++itr)
+	{
+		std::cout << *itr << std::endl;
+	}
+
+	std::vector<int>::iterator itr = vec.begin() + 2;
+	std::cout << "3 번째 원소 : " << *itr << std::endl;
+	return 0;
+}
