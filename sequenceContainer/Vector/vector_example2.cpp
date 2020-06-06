246aa0b40d280fcee6f060a01a9f4ef4c9caba8e
DoHyun Kim <dohyun9565@gmail.com>
2020-06-06 23:42:14 +0900

    vector의 iterator 사용 예시

--- sequenceContainer/Vector/vector_example2.cpp
+++ sequenceContainer/Vector/vector_example2.cpp
@@ -10,12 +10,13 @@ int main()
 	vec.push_back(30);
 	vec.push_back(40);
 
-	// 반복자 사용
+	// 반복자 사용해서 vector 컨테이너 요소를 순회할 수 있다.
 	for (std::vector<int>::iterator itr = vec.begin(); itr != vec.end(); ++itr)
 	{
 		std::cout << *itr << std::endl;
 	}
-
+        
+	// 반복자를 사용해서 vector 컨테이너에 접근할 수 있다.
 	std::vector<int>::iterator itr = vec.begin() + 2;
 	std::cout << "3 번째  : " << *itr << std::endl;
 	return 0;
