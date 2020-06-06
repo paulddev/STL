2caee81cb815f5110a93908a367a986faee33480
DoHyun Kim <dohyun9565@gmail.com>
2020-06-06 23:41:15 +0900

    push_back 의 간단한 예제

--- sequenceContainer/Vector/vector_example1.cpp
+++ sequenceContainer/Vector/vector_example1.cpp
@@ -5,11 +5,12 @@
 int main()
 {
 	std::vector<int> vec;
-	vec.push_back(10);
+	vec.push_back(10); // vec에 맨 뒤에 10을 삽입한다.
 	vec.push_back(20);
 	vec.push_back(30);
 	vec.push_back(40);
-
+        
+	// size_type 은 부호없는 정수이다.
 	for (std::vector<int>::size_type i = 0; i < vec.size(); i++)
 	{
 		std::cout << "vec 의 " << i + 1 << "  : " << vec[i] << std::endl;
