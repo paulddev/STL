e0a53defa8dd63bc2d8e8d5f538e36d49eefbcd3
DoHyun Kim <dohyun9565@gmail.com>
2020-06-06 23:51:51 +0900

    for문 기반의 출력과 range based for loop 기반의 출력 비교

--- sequenceContainer/Vector/vector_example8.cpp
+++ sequenceContainer/Vector/vector_example8.cpp
@@ -14,6 +14,8 @@ void print_vector(std::vector<T>& vec)
 template<typename T>
 void print_vector_range_based(std::vector<T>& vec)
 {
+	// const를 붙이면 이 부분에서는 수정을 하지 않겠다고 단언하는 것과 같다.
+	// 추가적으로 auto를 사용하면 알아서 자료형을 알맞게 맞춰준다는 점이다.
 	for (const auto& elem : vec)
 	{
 		std::cout << elem << std::endl;
