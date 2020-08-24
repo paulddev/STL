1fa9eaf6497a04262b924ede462b3c3843a5bd8c
DoHyun Kim <dohyun9565@gmail.com>
2020-08-24 15:24:36 +0900

    count(), find()

---
+++ 뇌를 자극하는 C++ STL/Part2/7-AssociateContainer/multimap/Part7-20.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main()
+{
+	multimap<int, int> mm;
+
+	mm.insert(pair<int, int>(5, 100));
+	mm.insert(pair<int, int>(3, 100));
+	mm.insert(pair<int, int>(8, 30));
+	mm.insert(pair<int, int>(3, 40));
+	mm.insert(pair<int, int>(1, 70));
+	mm.insert(pair<int, int>(7, 100));
+	mm.insert(pair<int, int>(8, 50));
+
+	multimap<int, int>::iterator iter;
+	for (iter = mm.begin(); iter != mm.end(); ++iter)
+		cout << "(" << iter->first << ", " << iter->second << ")" << " ";
+	cout << endl;
+
+	cout << "key 3의 원소의 개수: " << mm.count(3) << endl;
+
+	iter = mm.find(3);
+	if (iter != mm.end())
+		cout << "첫번째 key 3에 매핑된 value: " << iter->second << endl;
+
+	return 0;
+}
