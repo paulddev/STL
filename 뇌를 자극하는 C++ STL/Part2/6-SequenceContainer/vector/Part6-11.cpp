ec130abb79543e2ab3c72018e0ae639fad48dab1
DoHyun Kim <dohyun9565@gmail.com>
2020-08-24 15:50:19 +0900

    vector의 swap()

---
+++ 뇌를 자극하는 C++ STL/Part2/6-SequenceContainer/vector/Part6-11.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	vector<int> v1;
+	v1.push_back(10);
+	v1.push_back(20);
+	v1.push_back(30);
+
+	vector<int> v2;
+	v2.push_back(100);
+	v2.push_back(200);
+	v2.push_back(300);
+
+	for (vector<int>::size_type i = 0; i < v1.size(); ++i)
+		cout << v1[i] << ", " << v2[i] << endl;
+	cout << endl;
+
+	v1.swap(v2);
+	for (vector<int>::size_type i = 0; i < v1.size(); ++i)
+		cout << v1[i] << ", " << v2[i] << endl;
+	cout << endl;
+
+	return 0;
+}
