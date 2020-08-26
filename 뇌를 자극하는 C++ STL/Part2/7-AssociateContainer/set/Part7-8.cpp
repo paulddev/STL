1ee5659a485583044c014e528be25d9be02747f1
KimDoHyun <dohyun9565@gmail.com>
2020-08-26 15:29:29 +0900

    set의 정렬 기준을 사용한 값 비교

---
+++ 뇌를 자극하는 C++ STL/Part2/7-AssociateContainer/set/Part7-8.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main()
+{
+	set<int, less<int> > s;
+
+	// 30과 50의 비교
+	cout << (!s.key_comp()(30, 50) && !s.key_comp()(50, 30)) << endl;
+
+	// 30과 30의 비교
+	cout << (!s.key_comp()(30, 30) && !s.key_comp()(30, 30)) << endl;
+
+	return 0;
+}
