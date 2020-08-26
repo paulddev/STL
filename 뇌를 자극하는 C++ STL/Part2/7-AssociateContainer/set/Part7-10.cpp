33686ceb3937d51b4645d2e96afb1d33a1ec3d73
KimDoHyun <dohyun9565@gmail.com>
2020-08-26 15:30:18 +0900

    equal_range()

---
+++ 뇌를 자극하는 C++ STL/Part2/7-AssociateContainer/set/Part7-10.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main()
+{
+	set<int> s;
+
+	s.insert(50);
+	s.insert(30);
+	s.insert(80);
+	s.insert(40);
+	s.insert(10);
+	s.insert(70);
+	s.insert(90);
+
+	set<int>::iterator iter;
+	for (iter = s.begin(); iter != s.end(); ++iter)
+		cout << *iter << " ";
+	cout << endl;
+
+	pair<set<int>::iterator, set<int>::iterator> iter_pair;
+
+	iter_pair = s.equal_range(55);
+	cout << "lower: " << *iter_pair.first << endl;
+	cout << "upper: " << *iter_pair.second << endl << endl;
+
+
+	iter_pair = s.equal_range(30);
+	cout << "lower: " << *iter_pair.first << endl;
+	cout << "upper: " << *iter_pair.second << endl << endl;
+
+
+	if (iter_pair.first != iter_pair.second)
+		cout << "55가 s에 있다" << endl;
+	else
+		cout << "55가 s에 없다" << endl;
+
+	return 0;
+
+}
